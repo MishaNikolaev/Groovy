0210605e4f1f4a8feaf9fc058f4cc8a6c2f2c746
Mikhail Nikolaev <nmichail154@mail.ru>
2024-08-30 23:24:46 +0700

    Gradle implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 android {
@@ -66,4 +68,86 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.ui.text.google.fonts) // google fonts
+    implementation(libs.androidx.core.splashscreen) // splash screen
+
+    //Compose Navigation
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
+
+    //Paging 3
+    implementation ("androidx.paging:paging-runtime:3.3.2")
+    implementation ("androidx.paging:paging-compose:3.3.2")
+
+
+    // Coil
+    implementation(libs.coil.compose)
+
+    //Material
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
+
+    //Dagger Hilt
+    implementation(libs.hilt.android.v245)
+    kapt(libs.hilt.android.compiler.v245)
+    kapt(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    //AppDataStore
+    implementation(libs.androidx.datastore.preferences)
+
+    //Jsoup
+    implementation(libs.jsoup)
+
+    //Glide
+    implementation(libs.landscapist.glide)
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    //Bottom sheet
+    implementation(libs.androidx.compose.bom.v20230601)
+    implementation (libs.accompanist.pager)
+
+    // Architectural Components
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+
+    // Lifecycle
+    implementation (libs.androidx.lifecycle.extensions)
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
+    implementation ("androidx.lifecycle:lifecycle-runtime:2.8.4")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // Coroutine Lifecycle Scopes
+    implementation (libs.lifecycle.viewmodel.ktx.v220)
+    implementation (libs.lifecycle.runtime.ktx.v220)
+
+    // Navigation Component
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    // Glide
+    implementation ("com.github.bumptech.glide:glide:4.13.0")
+    kapt ("com.github.bumptech.glide:compiler:4.11.0")
+
+    // Activity KTX for viewModels()
+    implementation ("androidx.activity:activity-ktx:1.9.1")
+
+    // Timber
+    implementation ("com.jakewharton.timber:timber:5.0.1")
+
+    // Firebase Firestore
+    implementation ("com.google.firebase:firebase-firestore:25.1.0")
+
+    // Firebase Storage KTX
+    implementation ("com.google.firebase:firebase-storage-ktx:21.0.0")
+
+    // Firebase Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+
 }
